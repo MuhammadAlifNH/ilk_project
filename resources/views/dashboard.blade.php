946af5dc05edc198f38ed003c3202f7ed0720ee0
MuhammadAlifNH <muhammadalifnurhanif9504@google.com>
2025-02-10 10:57:08 +0700

    adjust

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,5 +7,21 @@
 @elseif(Auth::check()&& Auth::user()->type_users == 'teknisi')
     @include('dashboard.teknisi')
 @elseif(Auth::check())
-    <meta http-equiv="refresh" content="0; url={{ route('welcome') }}" />
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 @endif
